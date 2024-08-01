e57ac0d004cf3a597391f60c173bc9d83d8ee54c
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2024-08-01 22:10:53 +0300

    Health bar and tower connected, health info and damage method added.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -11,4 +11,5 @@ public class HealthBar : MonoBehaviour
     {
         slider.value = health; 
     }
+
 }
--- Assets/Scripts/TestTowerDefence.cs
+++ Assets/Scripts/TestTowerDefence.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class TestTowerDefence : MonoBehaviour
 {
+
+    private int maxHealth = 100;
+    private int currentHealth;
+    private HealthBar healthBar;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;  // When the game starts, the tower will have max health.
+        healthBar.SetHealth(maxHealth); //and health bar will show it.
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public int GetMaxHealth() { return maxHealth; }
+    public int GetCurrentHealth() { return currentHealth; }
+
+    public void TakaDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
     }
 }
