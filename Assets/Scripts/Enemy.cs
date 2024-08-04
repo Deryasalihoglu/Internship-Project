c78715ed5a852816617684a23964637e33abbad7
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2024-08-04 22:08:17 +0300

    Damage and damage interval variables added.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,11 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
+//using System.Collections;
+//using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
 
     public float speed;
+    public float damage;
+    public float damageInterval;
     [SerializeField] private Rigidbody2D rigidbody;
     [SerializeField] private Animator animator;
 
@@ -16,6 +18,8 @@ public class Enemy : MonoBehaviour
 
     void Start()
     {
+        damage = 5;
+        damageInterval = 0.21f;
         speed = 3;
         animator.SetFloat("speed", 3);
     }
@@ -27,7 +31,7 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Tower")
+        if (collision.gameObject.CompareTag("Tower"))
         {
             speed = 0;
             animator.SetFloat("speed", 0);
