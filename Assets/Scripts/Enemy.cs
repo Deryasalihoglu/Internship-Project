31349dc77a77f4375a8a9a1c48c9c6536487636e
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2024-08-06 21:33:38 +0300

    Attack method createdand called in update. It also controls goblin's moves.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,6 +8,9 @@ public class Enemy : MonoBehaviour
     public float speed;
     public float damage;
     public float damageInterval;
+    public bool isCollided;
+    private float timeSinceLastAttack;
+    private TestTowerDefence tower;
     [SerializeField] private Rigidbody2D rigidbody;
     [SerializeField] private Animator animator;
 
@@ -30,14 +33,38 @@ public class Enemy : MonoBehaviour
         rigidbody.MovePosition(transform.position + new Vector3(-speed * Time.fixedDeltaTime, 0f, 0f));
     }
 
+    void Update()
+    {
+        timeSinceLastAttack += Time.deltaTime;
+        if (timeSinceLastAttack > damageInterval && isCollided)
+        {
+            Attack();
+        }
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Tower"))
         {
+            tower = collision.gameObject.GetComponent<TestTowerDefence>();
+            isCollided = true;
             speed = 0;
             animator.SetFloat("speed", 0);
-            animator.Play("GoblinAttack_1");
         }
     }
 
+    void Attack()
+    {
+        if (!tower.isDestroyed)
+        {
+        animator.Play("GoblinAttack_1");
+        timeSinceLastAttack = 0;
+        tower.TakeDamage(damage);
+        }
+
+        else
+        {
+            animator.Play("GoblinIdle");
+        }
+    }
 }
