0b51f14148d80eff716016b834a2ee0dadb93043
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2024-08-14 16:58:44 +0300

    Class name is changed from TestTowerDefence to Tower and redundant lines deleted.

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] private HealthBar healthBar;
+    [SerializeField] private float maxHealth;
+    private float currentHealth;
+    public bool isDestroyed;
+    [SerializeField] private Animator animator;
+
+    void Start()
+    {
+        isDestroyed = false;
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetCurrentHealth(currentHealth);
+        Debug.Log("Damage taken is: " + damage);
+
+        if (currentHealth <= 0)
+        {
+            OnDeath();
+        }
+    }
+
+    public void OnDeath()
+    {
+        isDestroyed=true;
+        animator.Play("TowerExplosion");
+        Debug.Log("Tower has been destroyed");
+        GameController.Instance.OnTowerDestroyed();
+    }
+
+}
