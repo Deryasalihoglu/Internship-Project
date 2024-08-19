49f332c7e8f2e5ef222d376717b97089ee93bbdb
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2024-08-19 14:42:26 +0300

    Rigidbody and pool added, cooldown converted to lifeTime. arrowSpeed = 0 deleted to avoid making new arrows' speed 0.OnFired method created, it sets velocity and rotation when arrow be fired. SetPool defined.

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -1,20 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
     public float arrowSpeed = 5;
-    public float arrowDamage = 4;
-    public float arrowAttackCooldown = 3f;
+    public int arrowDamage = 4;
+    public float arrowLifeTime = 3f;
+    [SerializeField] private Rigidbody2D arrowRB;
+    private ArrowPool pool;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("enemy"))
         {
-            Enemy victimEnemy = collision.gameObject.GetComponent<Enemy>(); //!!!!!!!!!!!!!!!!!!!
-            arrowSpeed = 0;
+            Enemy victimEnemy = collision.gameObject.GetComponent<Enemy>();
             victimEnemy.TakeDamage(arrowDamage);
+            pool.AddInactiveArrowToThePool(this);
         }
+    }
+
+    public void OnFired(Vector3 direction)
+    {
+        arrowRB.velocity = direction.normalized * arrowSpeed;
+        transform.rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(0,0,-90);
+        transform.rotation = Quaternion.Euler(0, 0, transform.rotation.z); // HACK
+    }
+
+
+    public void SetPool(ArrowPool pool)
+    {
+        this.pool = pool;
     }
 }
