4b7aa3052df8bc1a3b2516464d23055b7f78026c
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2024-08-19 14:22:51 +0300

    Origin and container vars added to the constr. Get method updated to set start position of the arrow.

--- Assets/Scripts/ArrowPool.cs
+++ Assets/Scripts/ArrowPool.cs
@@ -1,33 +1,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArrowPool : MonoBehaviour
+public class ArrowPool
 {
     private Arrow arrowPrefab;
+    private GameObject originPoint;
+    private Transform arrowContainer;
     private Stack<Arrow> inactiveArrows = new Stack<Arrow>();
     private List<Arrow> activeArrows = new List<Arrow>();
 
-    public ArrowPool(Arrow arrow)
+    public ArrowPool(Arrow arrow, GameObject originPoint, Transform arrowContainer)
     {
         this.arrowPrefab = arrow;
+        this.originPoint = originPoint;
+        this.arrowContainer = arrowContainer;
     }
 
     public Arrow GetArrow()
     {
+        Arrow arrowFromPool;
         if (inactiveArrows.Count > 0)
         {
-            Arrow arrowFromPool = inactiveArrows.Pop();
-            arrowFromPool.gameObject.SetActive(true);
-            return arrowFromPool;
+            arrowFromPool = inactiveArrows.Pop();
+        }
+        else
+        {
+            arrowFromPool = Object.Instantiate(arrowPrefab, arrowContainer);
+            arrowFromPool.SetPool(this);
         }
 
-        Arrow newArrow = Object.Instantiate(arrowPrefab);
-        newArrow.gameObject.SetActive(true);
-        return newArrow;
+        arrowFromPool.transform.position = originPoint.transform.position;
+        arrowFromPool.gameObject.SetActive(true);
+        activeArrows.Add(arrowFromPool);
+        return arrowFromPool;
     }
 
     public void AddInactiveArrowToThePool(Arrow inactiveArrow)
     {
+        activeArrows.Remove(inactiveArrow);
         inactiveArrow.gameObject.SetActive(false);
         inactiveArrows.Push(inactiveArrow);
     }
