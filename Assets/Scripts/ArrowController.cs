b2137a91f6a3411fb20cc011581475a718802ce9
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2024-08-19 14:36:38 +0300

    Rigidbody deleted; pool, spawn time, origin point and container added. Pool defined in Start.Time control added to avoid firing new arrow before fired one becomes inactive. OnFired method called in Shoot after setting direction.

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
-    [SerializeField] Arrow arrow;
-    [SerializeField] private Rigidbody2D arrowRB;
     private Vector3 targetPoint;
+    private ArrowPool arrowPool;
+    private float timeSinceLastArrowSpawn;
+    [SerializeField] private float arrowCooldown = 0.5f;
+    [SerializeField] private Arrow arrow;
+    [SerializeField] private GameObject originPoint;
+    [SerializeField] private Transform arrowContainer;
+
+    private void Start()
+    {
+        arrowPool = new ArrowPool(arrow, originPoint, arrowContainer);
+    }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        timeSinceLastArrowSpawn += Time.deltaTime;
+        if (Input.GetMouseButtonDown(0) && timeSinceLastArrowSpawn > arrowCooldown)
         {
             ShootArrow();
         }
@@ -19,8 +29,11 @@ public class ArrowController : MonoBehaviour
     {
         targetPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         targetPoint.z = 0;
-        Vector3 direction = (targetPoint - transform.position).normalized;
-        arrow.GetComponent<Rigidbody2D>().velocity = direction * arrow.arrowSpeed;
-        //this.arrowRB.MovePosition(transform.position + new Vector3(10f * Time.fixedDeltaTime, 0f));
+        Vector3 origin = originPoint.transform.position;
+        origin.z = 0;
+        arrow = arrowPool.GetArrow();
+        Vector3 direction = targetPoint - origin;
+        arrow.OnFired(direction);
+        timeSinceLastArrowSpawn = 0;
     }
 }
