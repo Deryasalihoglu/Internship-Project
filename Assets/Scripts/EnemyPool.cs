191883e54eb3e1b8df871f784a035c9cedf65749
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2024-08-19 13:51:38 +0300

    Origin point and container vars defined. Defeated enemy removed from list in AddPool method. Get method updated to set start point of the enemies.

--- Assets/Scripts/EnemyPool.cs
+++ Assets/Scripts/EnemyPool.cs
@@ -4,35 +4,45 @@ using UnityEngine;
 public class EnemyPool
 {
     private Enemy enemyPrefab;
+    private GameObject enemyOriginPoint;
+    private Transform enemyContainer;
     private Stack<Enemy> inactiveEnemies = new Stack<Enemy>();
     private List<Enemy> activeEnemies = new List<Enemy>();
     private Tower targetTower;
 
-    public EnemyPool(Enemy enemyPrefab, Tower tower)
+    public EnemyPool(Enemy enemyPrefab, Tower tower, GameObject enemyOriginPoint, Transform enemyContainer)
     {
         this.enemyPrefab = enemyPrefab;
         this.targetTower = tower;
+        this.enemyOriginPoint = enemyOriginPoint; 
+        this.enemyContainer = enemyContainer;
     }
 
     public void AddDefeatedEnemyToPool(Enemy defeatedEnemy)
     {
-        defeatedEnemy.gameObject.SetActive(true);
+        activeEnemies.Remove(defeatedEnemy);
+        defeatedEnemy.gameObject.SetActive(false);
         inactiveEnemies.Push(defeatedEnemy);
     }
 
     public Enemy GetEnemy()
     {
+        Enemy enemyFromPool;
         if (inactiveEnemies.Count > 0)
         {
-            Enemy enemyFromPool = inactiveEnemies.Pop();
-            enemyFromPool.gameObject.SetActive(true);
-            enemyFromPool.tower = targetTower;
-            return enemyFromPool;
+            enemyFromPool = inactiveEnemies.Pop();
+        }
+        else
+        {
+            enemyFromPool = Object.Instantiate(enemyPrefab, enemyContainer);
+            enemyFromPool.SetPool(this);
         }
 
-        Enemy spawnedEnemy = Object.Instantiate(enemyPrefab);
-        spawnedEnemy.tower = targetTower;
-        activeEnemies.Add(spawnedEnemy);
-        return spawnedEnemy;
+        enemyFromPool.gameObject.SetActive(true);
+        enemyFromPool.transform.position = enemyOriginPoint.transform.position;
+        enemyFromPool.tower = targetTower;
+        activeEnemies.Add(enemyFromPool);
+        enemyFromPool.OnSpawned();
+        return enemyFromPool;
     }
 }
