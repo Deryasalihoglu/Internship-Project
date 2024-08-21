0ef074fbb44f0cf4f7c39ec263c9d02856752ad1
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2024-08-21 18:47:22 +0300

    Unused statements deleted.

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,13 +6,7 @@ public class HealthBar : MonoBehaviour
     public Slider slider;
     private float maxHealth;
 
-
-    void Start()
-    {
-
-    }
-
-    public void SetMaxHealth(float health) 
+    public void SetMaxHealth(float health)
     {
         maxHealth = health;
     }
