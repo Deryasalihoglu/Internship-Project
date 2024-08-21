2189f496482d92143e206ab8fbe0d9b03994ddd1
Derya Salihoglu <deryaslhoglu.54@gmail.com>
2024-08-21 18:44:57 +0300

    Arrow's moving direction corrected. An if condition added for arrows that landed on the ground.

--- Assets/Scripts/Arrow.cs
+++ Assets/Scripts/Arrow.cs
@@ -5,8 +5,8 @@ public class Arrow : MonoBehaviour
     public float arrowSpeed = 5;
     public int arrowDamage = 4;
     public float arrowLifeTime = 3f;
-    [SerializeField] private Rigidbody2D arrowRB;
     private ArrowPool pool;
+    [SerializeField] private Rigidbody2D arrowRB;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -16,13 +16,22 @@ public class Arrow : MonoBehaviour
             victimEnemy.TakeDamage(arrowDamage);
             pool.AddInactiveArrowToThePool(this);
         }
+
+        else if(collision.gameObject.CompareTag("Ground"))
+        {
+            pool.AddInactiveArrowToThePool(this);
+        }
     }
 
     public void OnFired(Vector3 direction)
     {
         arrowRB.velocity = direction.normalized * arrowSpeed;
-        transform.rotation = Quaternion.LookRotation(direction) * Quaternion.Euler(0,0,-90);
-        transform.rotation = Quaternion.Euler(0, 0, transform.rotation.z); // HACK
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+
+        //transform.rotation = Quaternion.LookRotation(direction);
+        //transform.rotation = Quaternion.Euler(0, 0, -90); // HACK
     }
 
 
